a0093d9ee143324147078c2a94ca0409858bbbb1
angelargd8 <angelargd8@outlook.com>
2023-02-18 15:33:12 -0600

    ac nodo uml

--- App_main/src/main/java/App_main/model/Stack/StackedDoubleLinkedList.java
+++ App_main/src/main/java/App_main/model/Stack/StackedDoubleLinkedList.java
@@ -5,5 +5,6 @@ public class StackedDoubleLinkedList<E> extends ListStack<E> implements Stack<E>
     public StackedDoubleLinkedList(){
         // TODO
         // this.data = new ArrayList<>();
+
     }
 }
