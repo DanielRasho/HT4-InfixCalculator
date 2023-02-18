0c170fb931d8c3cdfbde6e85d6b7b9a90d189db3
DiegoLinares11 <lin221256@uvg.edu.gt>
2023-02-18 17:42:19 -0600

    Nodo

---
+++ App_main/src/main/java/App_main/model/Stack/Nodo.java
@@ -0,0 +1,42 @@
+package App_main.model.Stack;
+
+public class Nodo<E>{
+    // clase nodo para las listas doblemente enlazadas/encadenadas
+    E dato;
+    Nodo<E> siguiente;
+    Nodo<E> anterior;
+    //constructor
+    public Nodo(E dato){
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+
+    }
+    //gets y sets
+    public void setDato(E dato){
+        this.dato = dato;
+    }
+
+    public E getDato(){
+        return dato;
+    }
+
+    public Nodo<E> setAnterior(Nodo<E> anterior){
+        this.anterior= anterior;
+        return anterior;
+    }
+
+    public Nodo<E> getAnterior(){
+        return anterior;
+    }
+
+    public Nodo<E> setSiguiente(Nodo<E> siguiente){
+        this.siguiente= siguiente;
+
+        return siguiente;
+    }
+
+    public Nodo<E> getSiguiente(){
+        return siguiente;
+    }
+}
