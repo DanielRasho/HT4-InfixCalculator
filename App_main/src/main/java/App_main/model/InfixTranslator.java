122701096e22853641e9bceed3edd52452eb505f
EJGDLG <93065287+EJGDLG@users.noreply.github.com>
2023-02-18 21:19:19 -0600

    Update InfixTranslator.java

--- App_main/src/main/java/App_main/model/InfixTranslator.java
+++ App_main/src/main/java/App_main/model/InfixTranslator.java
@@ -3,21 +3,25 @@ package App_main.model;
 import java.util.Stack;
 
 public class InfixTranslator {
-
+    // Método que convierte una expresión en notación infija a notación postfija
     public static String infixToPostFix(String expression) {
 
         Stack<Character> pila = new Stack<>();
         String postfix = "";
         for (int i = 0; i < expression.length(); i++) {
             char c = expression.charAt(i);
+            // Si es un dígito, lo agregamos directamente al resultado
             if (isOperator(c)) {
+                // Si es un operador, lo vamos agregando a la pila hasta encontrar uno con menor precedencia
                 while (!pila.isEmpty() && pila.peek() != '(' && precedence(pila.peek()) >= precedence(c)) {
                     postfix += pila.pop();
                 }
                 pila.push(c);
             } else if (c == '(') {
+                // Si es un paréntesis abierto, lo agregamos a la pila
                 pila.push(c);
             } else if (c == ')') {
+                // Si es un paréntesis cerrado, vamos sacando operadores de la pila hasta encontrar el paréntesis abierto correspondiente
                 while (!pila.isEmpty() && pila.peek() != '(') {
                     postfix +=" ";
                     postfix += pila.pop();
@@ -27,18 +31,19 @@ public class InfixTranslator {
                 postfix += c;
             }
         }
+        // Sacamos los operadores restantes de la pila y los agregamos al resultado
         while (!pila.isEmpty()) {
             postfix +=" ";
             postfix += pila.pop();
         }
-        // Cleaning extra whitespaces and returning.
+        
         return postfix.replaceAll(" +", " ");
     }
-
+    // Método que verifica si un carácter es un operador matemático
     public static boolean isOperator(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
     }
-
+     //Devuelve la precedencia de un operador matemático. Si el carácter no es un operador, devuelve 0.
     public static int precedence(char c) {
         if (c == '+' || c == '-') {
             return 1;
