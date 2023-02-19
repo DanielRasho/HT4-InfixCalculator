e2b58a320e13a4aeb0497c2673ffbd06da253b25
angelargd8 <angelargd8@outlook.com>
2023-02-18 19:38:42 -0600

    stacked double linked list

--- App_main/src/main/java/App_main/model/Stack/Nodo.java
+++ App_main/src/main/java/App_main/model/Stack/Nodo.java
@@ -1,24 +1,36 @@
 package App_main.model.Stack;
 
 public class Nodo<E>{
+    public E data;
     // clase nodo para las listas doblemente enlazadas/encadenadas
-    E dato;
+    //E data;
     Nodo<E> siguiente;
     Nodo<E> anterior;
     //constructor
-    public Nodo(E dato){
-        this.dato = dato;
+    /*public Nodo(){
+        this.data= data;
+        //this.data = new ArrayList<>();
+        //this.data = data;
         this.siguiente = null;
         this.anterior = null;
 
-    }
-    //gets y sets
-    public void setDato(E dato){
-        this.dato = dato;
+    }*/
+
+    public <E> Nodo(E data) {
+        //this.data= data;
+        //this.data = new ArrayList<>();
+        //this.data = data;
+        this.siguiente = null;
+        this.anterior = null;
     }
 
-    public E getDato(){
-        return dato;
+    //gets y sets
+    public void setData(E data){
+        this.data = data;
+    }
+
+    public E getData(){
+        return data;
     }
 
     public Nodo<E> setAnterior(Nodo<E> anterior){
--- App_main/src/main/java/App_main/model/Stack/StackedDoubleLinkedList.java
+++ App_main/src/main/java/App_main/model/Stack/StackedDoubleLinkedList.java
@@ -1,10 +1,87 @@
 package App_main.model.Stack;
-
+import java.util.ArrayList;
 public class StackedDoubleLinkedList<E> extends ListStack<E> implements Stack<E> {
+    //Nodo<E> nodo =new Node<>();
+    private static class Nodo<E>{
+        E data;
+        Nodo<E>  anterior;
+        Nodo<E> siguiente;
+
+        public Nodo(E data){
+            this.data = data; //(E) new ArrayList<>();
+            this.anterior=null;
+            this.siguiente=null;
+        }
+
+    }
+
+
+    int size;
+    /*Nodo<E>  anterior;
+    Nodo<E> siguientes;*/
+    // lo que se llama head y tail
+    Nodo<E> primero;
+    Nodo<E> ultimo;
 
     public StackedDoubleLinkedList(){
         // TODO
-        // this.data = new ArrayList<>();
+        //@rayo perdon esto no esta en ingles
+        this.data = new ArrayList<>();
+        //nodo.setDato(this.data);
+        size=0;
+        primero=null;
+        ultimo=null;
+        Nodo<E> anterior;
+        Nodo<E> siguiente;
 
     }
+    public int Size(){
+        return size;
+    }
+    //agregar xd
+    public void add(E data){
+        Nodo<E> nuevoNodo = new Nodo<>(data);
+        if (size==0){
+            primero=nuevoNodo;
+            ultimo=nuevoNodo;
+        }else{
+            ultimo.siguiente=nuevoNodo;
+            nuevoNodo.anterior= ultimo;
+            ultimo=nuevoNodo;
+        }
+        size++;
+    }
+
+
+    //eliminar
+    public E pop(int index){
+        if (index<0 ||index>=size){
+            throw new IndexOutOfBoundsException();
+        }
+
+        Nodo<E> actual= primero;
+        for (int i=0; i<index; i++){
+            actual=actual.siguiente;
+        }
+
+        E data =actual.data;
+
+        if (actual.anterior==null){
+            primero=actual.siguiente;
+        }else{
+            actual.anterior.siguiente=actual.siguiente;
+        }
+
+        if (actual.siguiente == null){
+            ultimo=actual.anterior;
+
+        }else{
+            actual.siguiente.anterior=actual.anterior;
+        }
+        size--;
+
+        return data;
+    }
+
+
 }
